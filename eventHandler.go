40df19c36c0ec33e8efd07c18d923172841244b8
orsonwang <orsonwang@gmail.com>
2016-05-17 12:26:00 +0800

    Regex Turning

--- eventHandler.go
+++ eventHandler.go
@@ -60,75 +60,75 @@ func (lbe *LineBotEventHandler) OnTextMessage(from, text string) {
 		"台外幣各類存款餘額"
 
 	switch {
-	case lbe.matchString("外幣.*利率.*", strAfterCut):
+	case lbe.matchString("外幣+.*利率+.*", strAfterCut):
 		strResult = "常用外幣利率表\n 美元 定存 2.3% 活存 1.8% \n 日圓 定存 0.1% 活存 0.1%"
 		break
-	case lbe.matchString("台幣.*利率.*", strAfterCut):
+	case lbe.matchString("台幣+.*利率+.*", strAfterCut):
 	case lbe.matchString("利率.*", strAfterCut):
 		strResult = "台幣活存利率表 \n 活存 0.5% 活儲 0.6% \n 定存\n 三個月 0.76% 六個月 0.78% 一年 0.80% 三年 0.80%\n https://www.skbank.com.tw/RAT/RAT2_TWSaving.aspx"
 		break
-	case lbe.matchString("美元|美金|USD.*歷史.*匯率.*", strAfterCut):
+	case lbe.matchString("(美元|美金|USD)+.*歷史+.*匯率+.*", strAfterCut):
 		strResult = "美元歷史匯率參考 http://tw.exchange-rates.org/history/TWD/USD/T"
 		break
-	case lbe.matchString("日圓|日元|日幣|JPY.*歷史.*匯率.*", strAfterCut):
+	case lbe.matchString("(日圓|日元|日幣|JPY)+.*歷史+.*匯率+.*", strAfterCut):
 		strResult = "日元歷史匯率參考 http://tw.exchange-rates.org/history/TWD/JPY/T"
 		break
-	case lbe.matchString("人民幣|RMB.*歷史.*匯率.*", strAfterCut):
+	case lbe.matchString("(人民幣|RMB)+.*歷史+.*匯率+.*", strAfterCut):
 		strResult = "人民幣歷史匯率參考 http://tw.exchange-rates.org/history/TWD/CNY/T"
 		break
-	case lbe.matchString("歷史.*匯率.*", strAfterCut):
+	case lbe.matchString("歷史+.*匯率+.*", strAfterCut):
 		strResult = "歷史匯率參考 http://tw.exchange-rates.org/history/TWD/USD/T"
 		break
-	case lbe.matchString("美元|美金|USD.*匯率.*", strAfterCut):
+	case lbe.matchString("(美元|美金|USD)+.*匯率+.*", strAfterCut):
 		strResult = "美元匯率\n" +
 			"現金買入 32.32000\n" +
 			"現金賣出 32.86200\n" +
 			"即期買入 32.62000\n" +
 			"即期賣出 32.72000"
 		break
-	case lbe.matchString("日圓|日元|日幣|JPY.*匯率.*", strAfterCut):
+	case lbe.matchString("(日圓|日元|日幣|JPY)+.*匯率+.*", strAfterCut):
 		strResult = "日圓匯率\n" +
 			"現金買入 0.29160\n" +
 			"現金賣出 0.30260\n" +
 			"即期買入 0.29800\n" +
 			"即期賣出 0.30200"
 		break
-	case lbe.matchString("人民幣|RMB.*匯率.*", strAfterCut):
+	case lbe.matchString("(人民幣|RMB)+.*匯率+.*", strAfterCut):
 		strResult = "人民幣匯率\n" +
 			"現金買入 4.89000\n" +
 			"現金賣出 5.05200\n" +
 			"即期買入 4.96200\n" +
 			"即期賣出 5.01200"
 		break
-	case lbe.matchString("匯率.*", strAfterCut):
+	case lbe.matchString("匯率+.*", strAfterCut):
 		strResult = ""
 		lbe.botClient.SendImage([]string{from},
 			"https://linebot.gaze.tw/exrate.png",
 			"https://linebot.gaze.tw/exrate.png")
 		break
 
-	case lbe.matchString("美元|美金|USD.*活存|存款.*餘額?.*", strAfterCut):
+	case lbe.matchString("(美元|美金|USD)+.*(活存|存款)+.*(餘額)?.*", strAfterCut):
 		strResult = "您的美元活存帳戶餘額為: 233,188.66 美元"
 		break
-	case lbe.matchString("日圓|日元|日幣|JPY.*活存|存款.*餘額?.*", strAfterCut):
+	case lbe.matchString("(日圓|日元|日幣|JPY)+.*(活存|存款)+.*(餘額)?.*", strAfterCut):
 		strResult = "您的日元活存帳戶餘額為: 233,188.66 日元"
 		break
-	case lbe.matchString("人民幣|RMB.*活存|存款.*餘額?.*", strAfterCut):
+	case lbe.matchString("(人民幣|RMB)+.*(活存|存款)+.*餘額?.*", strAfterCut):
 		strResult = "您沒有人民幣帳戶，若要開立請點連結 https://virtual.bank"
 		break
-	case lbe.matchString("美元|美金|USD.*定存|存單.*餘額?.*", strAfterCut):
+	case lbe.matchString("(美元|美金|USD)+.*(定存|存單)+.*(餘額)?.*", strAfterCut):
 		strResult = "您的美元定存帳戶餘額為: 1,000.00 美元"
 		break
-	case lbe.matchString("日圓|日元|日幣|JPY.*定存|存單.*餘額?.*", strAfterCut):
+	case lbe.matchString("(日圓|日元|日幣|JPY)+.*(定存|存單)+.*(餘額)?.*", strAfterCut):
 		strResult = "您沒有日元定存帳戶，若要開立請點連結 https://virtual.bank"
 		break
-	case lbe.matchString("人民幣|RMB.*定存|存單.*餘額?.*", strAfterCut):
+	case lbe.matchString("(人民幣|RMB)+.*(定存|存單)+.*(餘額)?.*", strAfterCut):
 		strResult = "您沒有人民幣帳戶，若要開立請點連結 https://virtual.bank"
 		break
-	case lbe.matchString("台幣?.*存款|活存|帳戶.*餘額?.*", strAfterCut):
+	case lbe.matchString("(台幣)?.*(存款|活存|帳戶)+.*(餘額)?.*", strAfterCut):
 		strResult = "您的台幣活存帳戶餘額為: 233,188.66 元\n "
 		break
-	case lbe.matchString("台幣?.*定存|存單.*餘額?.*", strAfterCut):
+	case lbe.matchString("(台幣)?.*(定存|存單)+.*(餘額)?.*", strAfterCut):
 		strResult = "您的台幣定存帳戶餘額為: 1,000,000.00 元\n"
 		break
 	}
